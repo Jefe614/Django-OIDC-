d1ae58129cb76f0cd19bef5670f91bc661922b32
jefe614 <jeffkimani32@gmail.com>
2024-07-22 18:04:23 +0300

    migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': dj_database_url.parse(
+        'postgresql://jefe_sample_user:gTcTWcGdwsugdmA9y66mlNTyFLndiYxm@dpg-cqf6seo8fa8c73elkkvg-a.oregon-postgres.render.com/jefe_sample'
+    )
 }
 
 
